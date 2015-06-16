7332827010723642ee84b5d7dc1e43cb219920e0
Taina Leal <tainacleal@gmail.com>
2015-06-16 11:51:35 -0300

    Creating a Task adapter to populate the list items

--- app/src/main/java/tainaleal/com/tasks/TaskListFragment.java
+++ app/src/main/java/tainaleal/com/tasks/TaskListFragment.java
@@ -2,7 +2,12 @@ package tainaleal.com.tasks;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,7 +24,34 @@ public class TaskListFragment extends ListFragment {
         getActivity().setTitle(R.string.app_name);
         mTasks = TaskBox.get(getActivity()).getTasks();
 
-        ArrayAdapter<Task> adapter = new ArrayAdapter<Task>(getActivity(), android.R.layout.simple_list_item_1, mTasks);
+        TaskAdapter adapter = new TaskAdapter(mTasks);
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Task t = ((TaskAdapter)getListAdapter()).getItem(position);
+    }
+
+    public class TaskAdapter extends ArrayAdapter<Task>{
+        public TaskAdapter(ArrayList<Task> tasks){
+            super(getActivity(), 0, tasks);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null){
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_task, null);
+            }
+
+            Task t = getItem(position);
+
+            TextView taskTitle = (TextView)convertView.findViewById(R.id.list_item_title);
+            taskTitle.setText(t.getTitle());
+
+            CheckBox taskCompleted = (CheckBox)convertView.findViewById(R.id.list_item_concluded);
+            taskCompleted.setChecked(t.isCompleted());
+            return convertView;
+        }
     }
 }
